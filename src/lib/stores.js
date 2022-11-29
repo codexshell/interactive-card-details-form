9ea288c00f8d1fcbbd57a2f1fd6c31f8c116162d
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2022-11-29 18:44:42 +0300

    export writable store input

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-const inputs = writable({
+export const inputs = writable({
 	cardNumber: '0000 0000 0000 0000',
 	cardHolder: 'Jane Appleseed',
 	month: '00',
